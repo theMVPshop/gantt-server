1bd8a2b35c8bece714167b0177a78b8068c7f98d
mayrant <mayrant2004@yahoo.com>
2022-03-31 18:48:30 -0500

    removed dummy user id from create user controller

--- controllers/users.js
+++ controllers/users.js
@@ -55,11 +55,6 @@ const getUser = (req, res) => {
 // }
 
 const createUser = (req, res) => {
-  let user = {}
-  const newId = users[users.length -1].user_id + 1
-  
-  
-  user.user_id = newId
   user.first_name = req.body.first_name
   user.last_name = req.body.last_name
   user.email = req.body.email
