6e2c383e1f517bb93b25fdcb002f5f64ef348296
Michael Fares <mjfares@gmail.com>
2022-04-06 18:08:34 -0500

    removed commented out old controller dummy data

--- controllers/courses.js
+++ controllers/courses.js
@@ -75,29 +75,6 @@ const createCourseForCohort = (req, res) => {
         if(err) return handleSQLError(res, err)
         return res.send(`New course created with id ${rows.insertId}`)
     })
-    // const course = {}
-    // const newId = courses[courses.length -1].course_id + 1
-
-    // course.course_id = newId
-    // course.cohort_id = req.body.cohort_id
-    // course.course_number = req.body.course_number
-    // course.hubspot_ticket = req.body.hubspot_ticket
-    // course.instructor = req.body.instructor
-    // course.teacher_assistant = req.body.teacher_assistant
-    // course.mode = req.body.mode
-    // course.course_link = req.body.course_link
-    // course.rocketchat = req.body.rocketchat
-    // course.start_date = req.body.start_date
-    // course.end_date = req.body.end_date
-    // course.location = req.body.location
-    // course.day_of_week = req.body.day_of_week
-    // course.active_status = req.body.active_status
-    // course.student_number_start = req.body.student_number_start
-    // course.student_number_end = req.body.student_number_end
-
-    // courses.push(course)
-    // res.json(courses)
-
 }
 
 const updateCourse = (req, res) => {
@@ -112,24 +89,6 @@ const updateCourse = (req, res) => {
         if(err) return handleSQLError(res, err)
         res.send(`Course with id ${id} updated`)
     })
-
-    // course.cohort_id = req.body.cohort_id
-    // course.course_number = req.body.course_number
-    // course.hubspot_ticket = req.body.hubspot_ticket
-    // course.instructor = req.body.instructor
-    // course.teacher_assistant = req.body.teacher_assistant
-    // course.mode = req.body.mode
-    // course.course_link = req.body.course_link
-    // course.rocketchat = req.body.rocketchat
-    // course.start_date = req.body.start_date
-    // course.end_date = req.body.end_date
-    // course.location = req.body.location
-    // course.day_of_week = req.body.day_of_week
-    // course.active_status = req.body.active_status
-    // course.student_number_start = req.body.student_number_start
-    // course.student_number_end = req.body.student_number_end
-
-  
 }
 
 const deleteCourse = (req, res) => {
