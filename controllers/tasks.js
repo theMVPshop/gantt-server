04c011cd52599264badfe5a1f42434da73ce0a53
JasonCornish5 <jason.cornish5@yahoo.com>
2022-05-10 18:31:32 -0500

    added a console log to updateTask controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,28 +1,27 @@
-const mysql = require('mysql')
-const pool = require('../sql/connection')
-const { handleSQLError } = require('../sql/error')
+const mysql = require("mysql");
+const pool = require("../sql/connection");
+const { handleSQLError } = require("../sql/error");
 
 const getTasks = (req, res) => {
-    let sql = `SELECT * FROM Tasks`
-    pool.query(sql, (err, rows) => {
-        if(err) return handleSQLError(res, err)
-        return res.json(rows)
-    })
-}
+  let sql = `SELECT * FROM Tasks`;
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
+};
 
 const getTask = (req, res) => {
-    let sql = `SELECT * FROM Tasks WHERE ?? = ?`
-    sql = mysql.format(sql, ["Tasks.task_id" , req.params.id])
+  let sql = `SELECT * FROM Tasks WHERE ?? = ?`;
+  sql = mysql.format(sql, ["Tasks.task_id", req.params.id]);
 
-    pool.query(sql, (err, rows) => {
-        if(err) return handleSQLError(res, err)
-        return res.json(rows)
-    })
-}
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
+};
 
 const createTask = (req, res) => {
-   
-    let sql = `INSERT INTO Tasks
+  let sql = `INSERT INTO Tasks
     (id,
     title,
     start_date,
@@ -38,60 +37,64 @@ const createTask = (req, res) => {
     active_status,
     student_number_start,
     student_number_end,
-    parent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+    parent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-    sql = mysql.format(sql, [ 
-        req.body.id,
-        req.body.title,
-        req.body.start_date,
-        req.body.end_date,
-        req.body.hubspot_ticket,
-        req.body.instructor, 
-        req.body.teacher_assistant, 
-        req.body.mode, 
-        req.body.course_link,
-        req.body.rocketchat,
-        req.body.location,
-        req.body.day_of_week,
-        req.body.active_status,
-        req.body.student_number_start,
-        req.body.student_number_end,
-        req.body.parent    
-    ])
+  sql = mysql.format(sql, [
+    req.body.id,
+    req.body.title,
+    req.body.start_date,
+    req.body.end_date,
+    req.body.hubspot_ticket,
+    req.body.instructor,
+    req.body.teacher_assistant,
+    req.body.mode,
+    req.body.course_link,
+    req.body.rocketchat,
+    req.body.location,
+    req.body.day_of_week,
+    req.body.active_status,
+    req.body.student_number_start,
+    req.body.student_number_end,
+    req.body.parent,
+  ]);
 
-    pool.query(sql, (err, result) => {
-        if(err) return handleSQLError(res, err)
-        return res.send(`New task created with id ${result.insertId}`)
-    })
-}
+  pool.query(sql, (err, result) => {
+    if (err) return handleSQLError(res, err);
+    return res.send(`New task created with id ${result.insertId}`);
+  });
+};
 
 const updateTask = (req, res) => {
-       const { id }  = req.params
-       const { body } = req
-       
-       let sql = 'UPDATE Tasks SET ? WHERE Tasks.id = ?'
-       sql = mysql.format(sql, [ body, id ])
-   
-       pool.query(sql, (err,rows) => {
-           if(err) return handleSQLError(res, err)
-           res.send(`Task with id ${id} updated`)
-       })
-}
+  const { id } = req.params;
+  const { body } = req;
+
+  console.log(body);
+
+  let sql = "UPDATE Tasks SET ? WHERE Tasks.id = ?";
+  sql = mysql.format(sql, [body, id]);
+
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    res.send(`Task with id ${id} updated`);
+  });
+};
 
 const deleteTask = (req, res) => {
-    let sql = 'DELETE FROM ?? WHERE ?? = ?'
-    sql = mysql.format(sql, ['Tasks', 'Tasks.id', req.params.id])
+  let sql = "DELETE FROM ?? WHERE ?? = ?";
+  sql = mysql.format(sql, ["Tasks", "Tasks.id", req.params.id]);
 
-    pool.query(sql, (err, rows) => {
-    if(err) return handleSQLError(err, res)
-    return res.send(`Deleted ${rows.affectedRows} task with id ${req.params.id}`)
- })
-}
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(err, res);
+    return res.send(
+      `Deleted ${rows.affectedRows} task with id ${req.params.id}`
+    );
+  });
+};
 
 module.exports = {
-    getTasks,
-    getTask,
-    createTask,
-    updateTask,
-    deleteTask
-}
+  getTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+};
